9b4a69f39557a11340473b0e2bbc11a9b45767fb
spkulgod <spkulgod@gmail.com>
2020-02-07 19:19:47 -0800

    forward function completed

--- PA3/basic_fcn.py
+++ PA3/basic_fcn.py
@@ -6,36 +6,38 @@ class FCN(nn.Module):
     def __init__(self, n_class):
         super().__init__()
         self.n_class = n_class
-        self.conv1   = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, dilation=1)
-        self.bnd1    = nn.BatchNorm2d(32)
-        self.conv2   = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, dilation=1)
-        self.bnd2    = nn.BatchNorm2d(64)
-        self.conv3   = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, dilation=1)
-        self.bnd3    = nn.BatchNorm2d(128)
-        self.conv4   = nn.Conv2d(128,256, kernel_size=3, stride=2, padding=1, dilation=1)
-        self.bnd4    = nn.BatchNorm2d(256)
-        self.conv5   = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, dilation=1)
-        self.bnd5    = nn.BatchNorm2d(512)
+        self.encoder = nn.Sequential(
+            self.conv1   = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, dilation=1),
+            self.bnd1    = nn.BatchNorm2d(32),
+            self.conv2   = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, dilation=1),
+            self.bnd2    = nn.BatchNorm2d(64),
+            self.conv3   = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, dilation=1),
+            self.bnd3    = nn.BatchNorm2d(128),
+            self.conv4   = nn.Conv2d(128,256, kernel_size=3, stride=2, padding=1, dilation=1),
+            self.bnd4    = nn.BatchNorm2d(256),
+            self.conv5   = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, dilation=1),
+            self.bnd5    = nn.BatchNorm2d(512)
+        )
         self.relu    = nn.ReLU(inplace=True)
-        self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn1     = nn.BatchNorm2d(512)
-        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn2     = nn.BatchNorm2d(256)
-        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn3     = nn.BatchNorm2d(128)
-        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn4     = nn.BatchNorm2d(64)
-        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn5     = nn.BatchNorm2d(32)
-        self.classifier = nn.Conv2d(32,34, kernel_size=1)
+        self.decoder = nn.Sequential(
+            self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1),
+            self.bn1     = nn.BatchNorm2d(512),
+            self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1),
+            self.bn2     = nn.BatchNorm2d(256),
+            self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1),
+            self.bn3     = nn.BatchNorm2d(128),
+            self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1),
+            self.bn4     = nn.BatchNorm2d(64),
+            self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1),
+            self.bn5     = nn.BatchNorm2d(32)
+        )
+        self.classifier = nn.Conv2d(32,self.n_class, kernel_size=1)
 
     def forward(self, x):
-        x1 = __(self.relu(__(x)))
-        # Complete the forward function for the rest of the encoder
-        
-        score = __(self.relu(__(out_encoder)))     
-        # Complete the forward function for the rest of the decoder
-        
+        x1 = self.relu(x)
+        out_encoder = self.encoder(x1)
+        encoded = self.relu(out_encoder)     
+        out_decoder = self.decoder(encoded)
         score = self.classifier(out_decoder)                   
 
         return score  # size=(N, n_class, x.H/1, x.W/1)
