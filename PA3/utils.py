be552df9077b86e9ee2ee0768bee2f793cb10be7
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-02-08 14:27:09 -0800

    added iou function and tested it

--- PA3/utils.py
+++ PA3/utils.py
@@ -1,18 +1,29 @@
 def iou(pred, target):
     """
+    Calulates the intersection over union between prediction and target.
+    Formula--> iou for class c = number of overlapping pixels / number of pixels of in class c in both prediction and target
+
     Pred: Tensor (B x W x H)
     Target: Tensor (B x W x H)
-    Returns: IOU accuracy
+    Returns: List of iou accuracies where index corresponds to class label.
     """
     ious = []
-    for cls in range(n_class):
-        # Complete this function
-        intersection = 0# intersection calculation
-        union = 0#Union calculation
+    for c in range(n_class):
+        # Converting pixels to T/F for class "c"
+        pred_class_n = pred == c #type bool
+        target_class_n = target == c #type bool
+        
+        # Intersection calculation is simply AND between tensors
+        intersection = (pred_class_n & target_class_n).sum().double()
+        #Union calculation is simpy OR between tensors
+        union = (pred_class_n + target_class_n).sum().double()
+
         if union == 0:
             ious.append(float('nan'))  # if there is no ground truth, do not include in evaluation
         else:
-            ious = []# Append the calculated IoU to the list ious
+            iou_for_class_c = intersection/union
+            ious.append(iou_for_class_c.item()) # Append the calculated IoU to the list ious
+
     return ious
 
 
