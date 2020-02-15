81b905ee82bdc77267618f15527c10dd9f529708
Sutej Kulgod <spkulgod@gmail.com>
2020-02-14 21:01:58 -0800

    iou2 updated

--- PA3/utils.py
+++ PA3/utils.py
@@ -49,12 +49,10 @@ def iou2(pred, target,n_class):
     Target: Tensor (B x W x H)
     Returns: List of iou accuracies where index corresponds to class label.
     """
-    ious = []
-    tot_int = 0
-    tot_union = 0
+    ints = []
+    unions = []
     for c in range(n_class):
         if(labels_classes[c].ignoreInEval):
-            ious.append(float('nan'))
             continue
         # Converting pixels to T/F for class "c"
         pred_class_n = pred == c #type bool
@@ -64,16 +62,10 @@ def iou2(pred, target,n_class):
         intersection = (pred_class_n & target_class_n).sum().double()
         #Union calculation is simpy OR between tensors
         union = (pred_class_n + target_class_n).sum().double()
-        tot_int += intersection
-        tot_union += union
-        if union == 0:
-            ious.append(float('nan'))
-            # ious.append(float('nan'))  # if there is no ground truth, do not include in evaluation
-        else:
-            iou_for_class_c = intersection/union
-            ious.append(iou_for_class_c.item()) # Append the calculated IoU to the list ious
+        ints.append(intersection)
+        unions.append(union)
 
-    return ious,tot_int/tot_union
+    return ints,unions
 
 
 def pixel_acc2(pred, target):
