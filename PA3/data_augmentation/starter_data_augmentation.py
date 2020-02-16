86b6d597385013fba7bb1dedd78091f03ce9a5b2
adqz1995 <ashahpur@eng.ucsd.edu>
2020-02-16 14:30:15 -0800

    added files to run model with data augmentation

---
+++ PA3/data_augmentation/starter_data_augmentation.py
@@ -0,0 +1,177 @@
+import sys
+sys.path.insert(0,'..') #adding parent directory to module search path
+
+from torchvision import utils
+from basic_fcn import *
+from dataloader_data_augmentation import *
+from utils import *
+import torchvision
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import time
+
+# Data augmentation is hard wired in the dataset class of dataloader_data_augmentation.py file
+# Inspired from - https://discuss.pytorch.org/t/how-to-apply-same-transform-on-a-pair-of-picture/14914/2
+
+train_dataset = CityScapesDataset(csv_file='../train.csv')
+val_dataset = CityScapesDataset(csv_file='../val.csv')
+test_dataset = CityScapesDataset(csv_file='../test.csv')
+train_loader = DataLoader(dataset=train_dataset,
+                          batch_size=2,
+                          num_workers=10,
+                          shuffle=True)
+val_loader = DataLoader(dataset=val_dataset,
+                          batch_size=2,
+                          num_workers=10,
+                          shuffle=True)
+test_loader = DataLoader(dataset=test_dataset,
+                          batch_size=2,
+                          num_workers=10,
+                          shuffle=True)
+
+
+def init_weights(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.xavier_uniform_(m.weight.data)
+        torch.nn.init.zeros_(m.bias.data)
+        
+epochs     = 55
+criterion = nn.CrossEntropyLoss()
+fcn_model = FCN(n_class=n_class)
+###changing class here
+# fcn_model = FCN_segnet(n_class=n_class) # ------> mod of encoder decoder class
+fcn_model.apply(init_weights)
+#fcn_model = torch.load('best_model')
+optimizer = optim.Adam(fcn_model.parameters(), lr=5e-3)
+
+
+use_gpu = torch.cuda.is_available()
+if use_gpu:
+    fcn_model = fcn_model.cuda()
+    
+def train():
+    losses = []
+    losses_val = []
+    p_accs = []
+    iou_accs = []
+    min_loss = float('inf')
+    for epoch in range(epochs+1):
+        fcn_model.train()
+        losses_epoch = []
+        ts = time.time()
+        for iter, (X, tar, Y) in enumerate(train_loader):
+            optimizer.zero_grad()
+
+            if use_gpu:
+                inputs = X.cuda()
+                labels = Y.cuda()
+            else:
+                inputs, labels = X.cpu(), Y.cpu()
+
+            outputs = fcn_model(inputs)
+            loss = criterion(outputs, labels)
+            losses_epoch.append(loss.item())
+            loss.backward()
+            optimizer.step()
+            
+            if iter % 400 == 0:
+                print("epoch{}, iter{}, loss: {}".format(epoch, iter, loss.item()))
+        
+        losses.append(np.mean(np.array(losses_epoch)))
+        print("Finish epoch {}, time elapsed {:.2f}, loss {:.3f}".format(epoch, time.time() - ts, losses[-1]))
+        losses_val.append(val(epoch))
+        if(min_loss>losses_val[-1]):
+            torch.save(fcn_model, 'best_model')
+            min_loss = losses_val[-1]
+        if epoch%5 == 0:
+            np.save("losses",np.array(losses))
+            np.save("losses_val",np.array(losses_val))
+
+    torch.save(fcn_model, 'final_model')
+    p_acc,iou_acc = val(epochs,False)
+    np.save("p_acc",np.array([p_acc]))
+    np.save("iou_acc",np.array([iou_acc]))
+    print("pixel accuracy", p_acc , "iou acc", iou_acc)
+
+def val(epoch,flag = True):
+    # Complete this function - Calculate loss, accuracy and IoU for every epoch
+    # Make sure to include a softmax after the output from your model
+    p_acc = 0
+    iou_acc = 0
+    iou_int = []
+    iou_union = []
+    count = 0
+    fcn_model.eval()
+    losses = []
+    for iter, (X, tar, Y) in enumerate(val_loader):
+        if use_gpu:
+            X = X.cuda()
+            tar = tar.cuda()
+            Y = Y.cuda()
+        else:
+            X,tar,Y = X.cpu(), tar.cpu(),Y.cpu()
+            
+        if flag:
+            with torch.no_grad():
+                outputs = fcn_model(X)
+            losses.append(criterion(outputs, Y).item())
+            continue
+        p, iou_i, iou_u = fcn_model.evaluate(X, tar,Y)
+        p_acc += p
+        iou_int.append(iou_i) 
+        iou_union.append(iou_u) 
+#         mask = np.logical_not(np.isnan(iou))
+#         iou_acc += np.mean(iou[mask])
+        count += 1
+    if flag:
+        return np.mean(np.array(losses))
+    iou_int = np.sum(np.array(iou_int),axis=0)
+    iou_union = np.sum(np.array(iou_union),axis=0)
+    iou_acc = np.mean(iou_int/iou_union)
+    print("Epoch {}: Pixel Acc: {}, IOU Acc: {}".format(epoch, p_acc/count, iou_acc))
+    return p_acc/count, iou_acc
+
+def test():
+    #Complete this function - Calculate accuracy and IoU 
+    # Make sure to include a softmax after the output from your model
+    p_acc = 0
+    iou_acc = 0
+    iou_int = []
+    iou_union = []
+    count = 0
+    for iter, (X, tar, Y) in enumerate(test_loader):
+        if use_gpu:
+            X = X.cuda()
+            tar = tar.cuda()
+            Y = Y.cuda()
+        else:
+            X,tar,Y = X.cpu(), tar.cpu(),Y.cpu()
+        p, iou_i, iou_u = fcn_model.evaluate(X, tar,Y)
+        p_acc += p
+        iou_int.append(iou_i) 
+        iou_union.append(iou_u) 
+        count += 1
+    iou_int = np.sum(np.array(iou_int),axis=0)
+    iou_union = np.sum(np.array(iou_union),axis=0)
+    iou_union += 1e-10
+    iou_acc = np.mean(iou_int/iou_union)
+    print("Test : Pixel Acc: {}, IOU Acc: {}".format( p_acc/count, iou_acc))
+    return p_acc/count, iou_acc
+      
+    
+if __name__ == "__main__":
+#    val(0)# show the accuracy before training
+#     test()
+    train()
+    print("Testing ")
+    pixel_accuracy, iou_accuracy = test()
+    print('pixel_accuracy = ', pixel_accuracy)
+    print('iou_accuracy = ', iou_accuracy)
+
+
+
+
+
+
+
