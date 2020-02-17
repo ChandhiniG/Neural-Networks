c6df297f76504c40e24dae20575cbf32a0900a58
adqz1995 <ashahpur@eng.ucsd.edu>
2020-02-16 21:39:01 -0800

    cleaned up code

--- PA3/data_augmentation/starter_data_augmentation.py
+++ PA3/data_augmentation/starter_data_augmentation.py
@@ -2,17 +2,30 @@ import sys
 sys.path.insert(0,'..') #adding parent directory to module search path
 
 from torchvision import utils
-from basic_fcn import *
-from dataloader_data_augmentation import *
-from utils import *
 import torchvision
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 import time
+import argparse
 
-# Data augmentation is hard wired in the dataset class of dataloader_data_augmentation.py file
-# Inspired from - https://discuss.pytorch.org/t/how-to-apply-same-transform-on-a-pair-of-picture/14914/2
+from basic_fcn import *
+from dataloader_data_augmentation import *
+from utils import *
+
+def init_weights(m):
+    '''
+    Initializing weight of Decoder layers only
+    '''
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.xavier_uniform_(m.weight.data)
+        torch.nn.init.zeros_(m.bias.data)
+
+# ------------- Please read. Thanks. You're awesome -------------
+# NOTE: Data augmentation is hard wired in the dataset class of dataloader_data_augmentation.py file. The transformations
+# had to be done manually so that the SAME random transformation is applied to the img and label.
+# Solution inspired from - https://discuss.pytorch.org/t/how-to-apply-same-transform-on-a-pair-of-picture/14914/2
+# ---------------------------------------------------------------
 
 train_dataset = CityScapesDataset(csv_file='../train.csv')
 val_dataset = CityScapesDataset(csv_file='../val.csv')
@@ -30,27 +43,22 @@ test_loader = DataLoader(dataset=test_dataset,
                           num_workers=10,
                           shuffle=True)
 
-
-def init_weights(m):
-    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-        torch.nn.init.xavier_uniform_(m.weight.data)
-        torch.nn.init.zeros_(m.bias.data)
-        
-epochs     = 60
+epochs    = 60
 criterion = nn.CrossEntropyLoss()
 fcn_model = FCN(n_class=n_class)
-###changing class here
-# fcn_model = FCN_segnet(n_class=n_class) # ------> mod of encoder decoder class
 fcn_model.apply(init_weights)
-#fcn_model = torch.load('best_model')
 optimizer = optim.Adam(fcn_model.parameters(), lr=5e-3)
-
 
 use_gpu = torch.cuda.is_available()
 if use_gpu:
     fcn_model = fcn_model.cuda()
     
 def train():
+    '''
+    This function trains the model, stores the traing and validation loss, 
+    stores the best model model every epoch, stores the losses every 5 epochs,
+    and saves the model at the end of the training irrespective of whether it was best or not
+    '''
     losses = []
     losses_val = []
     p_accs = []
@@ -95,8 +103,13 @@ def train():
     print("pixel accuracy", p_acc , "iou acc", iou_acc)
 
 def val(epoch,flag = True):
-    # Complete this function - Calculate loss, accuracy and IoU for every epoch
-    # Make sure to include a softmax after the output from your model
+    '''
+    This function tests the model using the validation set.
+    
+    The input epoch tells it at which epoch its performing validation.
+    If flag = True, it will only return validation loss
+    If flag = False, it will return pixel accuracy and iou accuracy
+    '''
     p_acc = 0
     iou_acc = 0
     iou_int = []
@@ -121,20 +134,20 @@ def val(epoch,flag = True):
         p_acc += p
         iou_int.append(iou_i) 
         iou_union.append(iou_u) 
-#         mask = np.logical_not(np.isnan(iou))
-#         iou_acc += np.mean(iou[mask])
         count += 1
     if flag:
         return np.mean(np.array(losses))
     iou_int = np.sum(np.array(iou_int),axis=0)
     iou_union = np.sum(np.array(iou_union),axis=0)
+    iou_union += 1e-10 #to avoid zero division error
     iou_acc = np.mean(iou_int/iou_union)
     print("Epoch {}: Pixel Acc: {}, IOU Acc: {}".format(epoch, p_acc/count, iou_acc))
     return p_acc/count, iou_acc
 
 def test():
-    #Complete this function - Calculate accuracy and IoU 
-    # Make sure to include a softmax after the output from your model
+    '''
+    Function to test the model and output the pixel and iou accuracy
+    '''
     p_acc = 0
     iou_acc = 0
     iou_int = []
@@ -154,24 +167,22 @@ def test():
         count += 1
     iou_int = np.sum(np.array(iou_int),axis=0)
     iou_union = np.sum(np.array(iou_union),axis=0)
-    iou_union += 1e-10
+    iou_union += 1e-10 #to avoid zero division error
     iou_acc = np.mean(iou_int/iou_union)
     print("Test : Pixel Acc: {}, IOU Acc: {}".format( p_acc/count, iou_acc))
     return p_acc/count, iou_acc
       
     
 if __name__ == "__main__":
-#    val(0)# show the accuracy before training
-#     test()
-    train()
-    print("Testing ")
-    pixel_accuracy, iou_accuracy = test()
-    print('pixel_accuracy = ', pixel_accuracy)
-    print('iou_accuracy = ', iou_accuracy)
-
-
-
-
-
-
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', required = True)
+    args = parser.parse_args()
+    
+    if args.mode == "train":
+        train()
+    else:
+        print("Testing ")
+        model = torch.load('best_model')
+        pixel_accuracy, iou_accuracy = test()
+        print('pixel_accuracy = ', pixel_accuracy)
+        print('iou_accuracy = ', iou_accuracy)
