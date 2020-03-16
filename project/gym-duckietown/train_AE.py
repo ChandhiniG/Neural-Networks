c100009d3ada7bda737896485018715719a7d9e0
Sutej Kulgod <spkulgod@gmail.com>
2020-03-15 19:53:13 -0700

    data added

--- project/gym-duckietown/train_AE.py
+++ project/gym-duckietown/train_AE.py
@@ -43,6 +43,47 @@ optimizer = optim.Adam(model.parameters(), lr=2e-4)
 model.to(device)
 criterion.to(device)
 
+env = launch_env()
+print("Initialized environment")
+
+# Wrappers
+env = ResizeWrapper(env)
+env = NormalizeWrapper(env)
+env = ImgWrapper(env) # to make the images from 160x120x3 into 3x160x120
+env = ActionWrapper(env)
+env = DtRewardWrapper(env)
+print("Initialized Wrappers")
+
+
+def get_train_data(size):
+    # Launch the env with our helper function
+    replay_buffer = ReplayBuffer(size)
+    obs = env.reset()
+    done = False
+    
+    print('Start: Collecting train data')
+    ts = time.time()
+    for i in range(int(size)):
+        
+        if i%10==0 or done:
+            obs = env.reset()
+            done = False
+        else:
+            action = env.action_space.sample()
+            obs, _, done, _ = env.step(action)
+        
+        replay_buffer.add(obs.astype(float), 0, 0, 0, 0)
+    
+    print('End: Collecting train data. Time taken: {:.2f}'.format(time.time() - ts))
+    return replay_buffer
+
+for i in range(1000):    
+    num_train_images = int(40)
+    replay_buffer = get_train_data(num_train_images)
+    num_val_images = int(10)
+    replay_buffer_val = get_train_data(num_val_images)
+    np.savez_compressed('data/compressed'+str(i), val=replay_buffer_val, train=replay_buffer)
+
 def train():
     
     
@@ -51,18 +92,18 @@ def train():
         os.makedirs(model_dir)
 
     losses = []
-    model.train()
+    val_losses = []
     min_loss = float('inf')
-    num_train_images = int(1e2)
-    replay_buffer = get_train_data(num_train_images)
     batch_size = 20
     
     for epoch in range(epochs):
+        model.train()
         loss_batch = []
         shuffled_indices = list(range(num_train_images))
         random.shuffle(shuffled_indices)
         ts = time.time()
         for i in range(0, num_train_images, batch_size):
+            optimizer.zero_grad()
             # Making batch
             batch_indices = shuffled_indices[i: i+batch_size]
             obs_batch = [replay_buffer.storage[j][0] for j in batch_indices]
@@ -94,48 +135,34 @@ def train():
 #                 print("epoch{}, batch: {}, loss: {}".format(epoch, i, loss_batch[-1]))
         
         losses.append(np.mean(np.array(loss_batch)))
-        print("Finish epoch {}, time elapsed {:.2f}, loss: {:.3f}".format(epoch, time.time() - ts, losses[-1]))
+        val_losses.append(val())
+        print("Finish epoch {}, time elapsed {:.2f}, loss: {:.3f}, val_loss{:.3f}".format(epoch, time.time() - ts, losses[-1], val_losses[-1]))
         
-        if epoch%50==0 and losses[-1] < min_loss:
+        if epoch%50==0 or losses[-1] < min_loss:
             save_model(model, model_dir, 'model_best')
-            min_loss = losses[-1]
+            min_loss = val_losses[-1]
     
     np.save("CAE_losses",np.array(losses))
     save_model(model, model_dir, 'model_final')
 
-def get_train_data(size):
-    # Launch the env with our helper function
-    env = launch_env()
-    print("Initialized environment")
+def val():
+    batch_size = 10
+    model.eval()
+    val_batch = []
+    shuffled_indices = list(range(num_val_images))
+    for i in range(0, num_val_images, batch_size):
+        # Making batch
+        batch_indices = shuffled_indices[i: i+batch_size]
+        obs_batch = [replay_buffer.storage[j][0] for j in batch_indices]   
+        obs = np.stack(obs_batch, axis=0)
+        obs = torch.from_numpy(obs).float()
+        obs = obs.to(device)
+        # Feed forward
+        output = model(obs)
+        loss   = criterion(output, obs)
+        val_batch.append(loss.item())
+    return np.mean(np.array(val_batch))
 
-    # Wrappers
-    env = ResizeWrapper(env)
-    env = NormalizeWrapper(env)
-    env = ImgWrapper(env) # to make the images from 160x120x3 into 3x160x120
-    env = ActionWrapper(env)
-    env = DtRewardWrapper(env)
-    print("Initialized Wrappers")
-
-    replay_buffer = ReplayBuffer(size)
-    obs = env.reset()
-    done = False
-    
-    print('Start: Collecting train data')
-    ts = time.time()
-    for i in range(int(size)):
-        
-        if i%10==0 or done:
-            obs = env.reset()
-            done = False
-        else:
-            action = env.action_space.sample()
-            obs, _, done, _ = env.step(action)
-        
-        replay_buffer.add(obs.astype(float), 0, 0, 0, 0)
-    
-    print('End: Collecting train data. Time taken: {:.2f}'.format(time.time() - ts))
-    return replay_buffer
-    
 if __name__ == '__main__':
     train()
     
